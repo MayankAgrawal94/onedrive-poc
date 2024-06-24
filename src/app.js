b8825be05f0b8e49c9f7bf5379a77647bf92783f
MayankAgrawal94 <mayankagrawal4949@gmail.com>
2024-06-24 21:58:18 +0530

    Code Refactor

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressSession = require('./config/session.config');
 const http = require('node:http');
+const path = require('node:path');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,25 +13,21 @@ app.use(bodyParser.json());
 
 app.use(expressSession);
 
-// require('./config/db')
-
 const ApiV1 = 'v1'
 const { isAuthenticated } = require('./middleware/isAuthenticated')
 
 // Importing routes
 const msOauth2 = require('./app/routes/auth/msAuth.routes')
 const basicAuth = require('./app/routes/auth/basicAuth.routes')
-const msOnedrive = require('./app/routes/msOneDrive.routes')
+const msOnedrive = require('./app/routes/msOneDrive.routes');
+
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/public/index.html`))
-app.get('/public/:path1/:path2?', (req,res) => {
-    const { path1, path2 } = req.params
-    res.sendFile(`${__dirname}/public${path1 ? '/'+path1 : ''}${path2 ? '/'+path2 : ''}`)
-})
 
 app.get('/welcome', isAuthenticated, (req, res) => {
-    // console.log("req.session.user ==>", req.session.user )
-    res.sendFile(`${__dirname}/public/welcome.html`);
+    res.sendFile(path.join(__dirname, 'public', 'welcome.html'));
 });
 
 //Microsoft OAuth2
